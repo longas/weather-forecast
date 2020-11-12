d3e65264fcb760034b4e26aa121a604825c2a84d
Gabriel Longás <glongas@gmail.com>
2020-11-12 21:17:18 +0100

    TailwindCSS setup

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h2 className="text-red-500">Titular</h2>
+      </main>
+    </div>
+  );
+}
