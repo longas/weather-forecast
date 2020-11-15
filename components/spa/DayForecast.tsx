82eb17dcf07e1a282997130fbf9d6005722ff9b0
Gabriel Longás <glongas@gmail.com>
2020-11-15 16:45:45 +0100

    Hourly forecast chart

--- components/spa/DayForecast.tsx
+++ components/spa/DayForecast.tsx
@@ -1,5 +1,6 @@
 import React, { useMemo } from "react";
 import { useSelector } from "react-redux";
+import { Line, LineChart, ResponsiveContainer, Tooltip } from "recharts";
 import { IStore } from "../../redux/store";
 import { Hour } from "../../types";
 import DayForecastListItem from "./DayForecastListItem";
@@ -15,11 +16,33 @@ const DayForecast = () => {
     []
   );
 
+  const chartData = useMemo(() => {
+    if (!hours.length) return [];
+    return hours.map((h, i) => ({
+      name: `Test ${i}`,
+      temperature: h.temperature,
+    }));
+  }, [hours]);
+
   return hours.length > 0 ? (
     <div className="mt-4 bg-white border border-gray-400 shadow-md rounded">
       <h4 className="pt-4 pl-5 font-extrabold capitalize">{selectedDay}</h4>
 
-      <ul className="mt-2 grid grid-cols-6">
+      <div className="mt-6 px-6">
+        <ResponsiveContainer width="100%" height={75}>
+          <LineChart data={chartData}>
+            <Tooltip />
+            <Line
+              isAnimationActive={false}
+              type="monotone"
+              dataKey="temperature"
+              stroke="black"
+            />
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
+
+      <ul className="mt-5 grid grid-cols-6">
         {dayHours.map((h, i) => {
           const hourString = `${h.toString().padStart(2, "0")}:00`;
           const hourData = hours[i];
