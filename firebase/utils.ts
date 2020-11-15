b6dd4925685365831934e38741365808d983c820
Gabriel Longás <glongas@gmail.com>
2020-11-15 21:50:31 +0100

    Update Firebase ENV vars

--- firebase/utils.ts
+++ firebase/utils.ts
@@ -4,11 +4,20 @@ export function getFirestore() {
   // Check if the firebase app has been already initialized
   // this is needed for some serverless function enviroments
   // and in the dev mode due to hot-reloading.
+
   if (admin.apps.length === 0) {
-    admin.initializeApp({
-      credential: admin.credential.applicationDefault(),
-      databaseURL: "https://adidas-6179b.firebaseio.com",
-    });
+    try {
+      admin.initializeApp({
+        credential: admin.credential.cert({
+          projectId: process.env.FIREBASE_PROJECT_ID,
+          clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+          privateKey: process.env.FIREBASE_PRIVATE_KEY,
+        }),
+      });
+    } catch (e) {
+      console.log(e);
+      process.exit(0);
+    }
   }
 
   return admin.firestore();
